9c7b60fb603cfcec7f422ad0b459f552b6612ecf
odilovaslanbek20 <aslanbekodilov524@gmail.com>
2025-01-11 23:28:30 +0500

    n87

---
+++ 1-dars/1-dars ..../main.js
@@ -0,0 +1,19 @@
+let olmaKg = 5;
+let olmaNarxi = 50000;
+let qiymat = olmaKg * olmaNarxi;
+console.log(`Sizdan ${qiymat}ming so'm bo'ladi...`);
+
+let nokKg = 8;
+let nokNarxi = 90000;
+let qiymat1 = nokKg * nokNarxi;
+console.log(`Sizdan ${qiymat1}ming so'm bo'ladi...`);
+
+let olchaKg = 4;
+let olchaNarxi = 80000;
+let qiymat2 = olchaKg * olchaNarxi;
+console.log(`Sizdan ${qiymat2}ming so'm bo'ladi...`);
+
+let liviKg = 6;
+let liviNarxi = 10000;
+let qiymat3 = liviKg * liviNarxi;
+console.log(`Sizdan ${qiymat3}ming so'm bo'ladi...`);
