5faaf005298af201d12b498d5f06a402ca7f5526
odilovaslanbek20 <aslanbekodilov524@gmail.com>
2025-02-16 15:57:31 +0500

    N-87

---
+++ 5-oy/2-dars/2-dars uy-vazifam/JS/main.js
@@ -0,0 +1,55 @@
+let darkMode = false; 
+let themeImage = document.getElementById("themeImage");
+let toggleBtn = document.getElementById("toggleBtn");
+let body = document.body;
+let icon = document.getElementById("icon");
+
+let mediaQuery = window.matchMedia("(max-width: 520px)");
+
+let bars = document.getElementById("bars");
+let ext = document.getElementById("ext");
+let menyu = document.querySelector(".menyu");
+
+bars.addEventListener("click", () => {
+  menyu.classList.toggle("active");
+})
+
+ext.addEventListener("click", () => {
+  menyu.classList.toggle("active");
+})
+
+function updateImage() {
+  if (mediaQuery.matches) {
+    themeImage.src = darkMode ? "./img/LogoWeb.png" : "./img/LogoWeb.png";
+  } else {
+    themeImage.src = darkMode ? "./img/NajotTalimLogo.png" : "./img/najotLogo.png";
+  }
+}
+
+function updateResponsiveStyles() {
+  if (window.innerWidth < 520) {
+    themeImage.style.width = "180px";
+  } else {
+    themeImage.style.width = "200px";
+  }
+}
+
+
+toggleBtn.addEventListener("click", () => {
+  darkMode = !darkMode;
+  body.classList.toggle("dark-mode");
+  icon.innerHTML = darkMode ? "🌙" : "☀️"; 
+  updateImage(); 
+  fnBg()
+});
+
+function fnBg() {
+  darkMode = !darkMode;
+  menyu.classList.toggle("dark-mode");
+}
+
+window.addEventListener("resize", updateResponsiveStyles);
+mediaQuery.addEventListener("change", updateImage);
+
+updateResponsiveStyles();
+updateImage();
